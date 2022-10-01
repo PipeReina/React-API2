a8b8c2b30651ad8fb1e07f018177850844f64d61
Your Name <you@example.com>
2022-10-01 10:15:39 -0500

    dash-form_terminados

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './index.css';
-import Home from './pages/homee/home';
-import reportWebVitals from './reportWebVitals';
+import Dashboard from './pages/dashboard/Dashboard';
+import Registrar from './pages/registrar/Registrar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path='/dashboard' element={<Dashboard />} />
+      <Route path='/registrar' element={<Registrar />} />
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/pages/dashboard/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Tablero from '../../components/dashboard/Dashboard';
+import './Dashboard.css';
+
+function Dashboard() {
+    return (
+        <div className="contenedor">
+            <Tablero />
+        </div>
+    );
+}
+
+export default Dashboard;
